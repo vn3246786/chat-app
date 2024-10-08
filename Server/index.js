22c500cb04a7db3f3fadd220da329c75c1e7c0a4
viraj naik <vn3246786@gmail.com>
2024-10-08 16:54:37 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ app.use(express.json())
 const server = createServer(app)
 const io = new Server(server,{
    cors:{
-    origin:["http://localhost:5173"]
+    origin:["http://localhost:5173","http://localhost:5174","http://localhost:5175"]
    } 
 })
 
