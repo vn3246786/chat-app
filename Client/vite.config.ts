90319551628602229d7eac32555861cc3dd03346
viraj naik <vn3246786@gmail.com>
2024-10-09 10:14:28 +0530

    update

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,16 +1,16 @@
-import { defineConfig, loadEnv,} from 'vite'
+import { defineConfig, } from 'vite'
 import react from '@vitejs/plugin-react'
 import {config} from "dotenv"
 
 config()
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode})=>{
-  const env = loadEnv(mode,process.cwd(),"VITE_")
+export default defineConfig(({})=>{
+  
  return {
     server:{
       proxy:{
-        "/api":`${env.VITE_SERVER_URL}`
+        "/api":`${process.env.VITE_SERVER_URL}`
       }
     },
     plugins: [react()],
