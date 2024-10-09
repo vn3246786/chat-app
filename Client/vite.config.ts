573b6dfd91e1a192a2271cb429b930b435f48aaa
viraj naik <vn3246786@gmail.com>
2024-10-09 10:32:00 +0530

    update

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -5,14 +5,13 @@ import {config} from "dotenv"
 config()
 
 // https://vitejs.dev/config/
-export default defineConfig(({})=>{
+export default defineConfig({
   
- return {
     server:{
       proxy:{
         "/api":`https://chat-app-fve1.onrender.com`
       }
     },
     plugins: [react()],
-  }
+  
 })
