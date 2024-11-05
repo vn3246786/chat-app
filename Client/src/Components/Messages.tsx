69e46e097eaffe222f4acd9374c393775a7453a1
viraj naik <vn3246786@gmail.com>
2024-11-05 06:01:31 +0530

    update

--- Client/src/Components/Messages.tsx
+++ Client/src/Components/Messages.tsx
@@ -2,15 +2,13 @@ import { messages } from "../TypeDefinitions"
 
 const Messages = ({data}:{data:messages}) => {
 
-  const sentStyles = "w-fit flex gap-2  bg-green-500 px-[10px] py-[2px] rounded-lg mb-[10px] ml-auto"
-  const recievedStyles = "w-fit bg-white px-[10px] py-[2px] rounded-lg mb-[10px]"
  
   return (
     <div className="bg-gray-400 min-h-[93vh] pb-[50px] w-[100vw] pt-[10px] px-[10px]">
       {data.map((msg,i)=>{ 
       return (msg.type==="sent"?
-      <div key={i} className={sentStyles}>{msg.message}</div>:
-         <div key={i} className={recievedStyles}>{msg.message}</div> )
+      <div key={i} className="w-[50%] bg-green-500 px-[10px] py-[2px] rounded-lg mb-[10px] ml-auto break-words">{msg.message}</div>:
+         <div key={i} className="w-[50%] bg-white px-[10px] py-[2px] rounded-lg mb-[10px] break-words">{msg.message}</div> )
       })} 
 
     </div>
